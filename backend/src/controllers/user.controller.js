a8c477de07b6809a0f95f3f4efcc830926518824
gJethro <131506858+gJethro@users.noreply.github.com>
2024-05-21 15:23:08 -0500

    Recorte de líneas

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { createAccessToken } = require('../libs/jwt.js');
+
 const { 
     Usuario,
     Genero,
@@ -5,89 +9,52 @@ const {
     UnidadMedida 
 } = require('../model/models.js');
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const { createAccessToken } = require('../libs/jwt.js');
-
 const register = async (req, res) => {
     try {
-
         const {
-            num_doc,
-            nombre,
-            apellido,
-            fecha_de_nacimiento,
-            generos_id,
-            email,
-            password,
-            altura,
-            peso,
-            contacto_emergencia,
-            unidades_medida
+            num_doc, nombre, apellido,
+            fecha_de_nacimiento, generos_id,
+            email, password, altura, peso,
+            contacto_emergencia, unidades_medida
         } = req.body;
-
         
         const hashPass = await bcrypt.hash(password, saltRounds);
-
-        // Validar que los campos requeridos estén presentes
-        if (!num_doc || !nombre || !apellido || !fecha_de_nacimiento || !generos_id || !email || !password || !altura || !peso || !contacto_emergencia || !unidades_medida) {
-            return res.status(400).json({ message: 'Todos los campos son obligatorios' });
-        }
-
-        // Verificar si el género existe
+        if (
+            !num_doc || !nombre || !apellido || 
+            !fecha_de_nacimiento || !generos_id || 
+            !email || !password || !altura || !peso || 
+            !contacto_emergencia || !unidades_medida
+        ) return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+        
         const genero = await Genero.findByPk(generos_id);
-        if (!genero) {
-            return res.status(404).json({ message: 'Género no encontrado' });
-        }
-
-        // Crear o verificar el contacto de emergencia
+        if (!genero) return res.status(404).json({ message: 'Género no encontrado' });
+        
         let emergencyContact = await ContactoEmergencia.findByPk(contacto_emergencia.num_doc);
-        if (!emergencyContact) {
-            emergencyContact = await ContactoEmergencia.create(contacto_emergencia);
-        } else {
-            await emergencyContact.update(contacto_emergencia);
-        }
+        if (!emergencyContact) emergencyContact = await ContactoEmergencia.create(contacto_emergencia);
+        else await emergencyContact.update(contacto_emergencia);
 
-        // Crear o verificar la unidad de medida
         let unitsConfig = await UnidadMedida.findOne({
             where: {
-                unidad_longitud: unidades_medida.unidad_longitud,
-                unidad_peso: unidades_medida.unidad_peso,
-                unidad_presion_arterial: unidades_medida.unidad_presion_arterial,
-                unidad_glucosa_sangre: unidades_medida.unidad_glucosa_sangre,
-                unidad_frecuencia_cardiaca: unidades_medida.unidad_frecuencia_cardiaca,
-                unidad_temperatura: unidades_medida.unidad_temperatura
+                unidad_longitud: unidades_medida.unidad_longitud, unidad_peso: unidades_medida.unidad_peso,
+                unidad_presion_arterial: unidades_medida.unidad_presion_arterial, unidad_glucosa_sangre: unidades_medida.unidad_glucosa_sangre,
+                unidad_frecuencia_cardiaca: unidades_medida.unidad_frecuencia_cardiaca,unidad_temperatura: unidades_medida.unidad_temperatura
             }
-        });
+        }); if (!unitsConfig) unitsConfig = await UnidadMedida.create(unidades_medida);
+        
+        const newUser = new Usuario({
+            num_doc, nombre, apellido,
+            fecha_de_nacimiento, generos_id,
+            email, password: hashPass, altura, peso,
+            contacto_emergencia_num_doc: emergencyContact.num_doc, unidades_medida_id: unitsConfig.id
+        }); const savedUser = await newUser.save();
 
-        if (!unitsConfig) {
-            unitsConfig = await UnidadMedida.create(unidades_medida);
-        }
-
-
-        // Crear el usuario
-        const newUser = await Usuario.create({
-            num_doc,
-            nombre,
-            apellido,
-            fecha_de_nacimiento,
-            generos_id,
-            email,
-            password: hashPass,
-            altura,
-            peso,
-            contacto_emergencia_num_doc: emergencyContact.num_doc,
-            unidades_medida_id: unitsConfig.id
-        });
-
-        const token = await createAccessToken({ num_doc: newUser.num_doc });
+        const token = await createAccessToken({ num_doc: savedUser.num_doc });
         res.cookie('token', token);
         
         const response = {
             num_doc: newUser.num_doc,
             nombre_completo: `${newUser.nombre} ${newUser.apellido}`,
-            genero: genero.nombre,
-            email: newUser.email,
+            genero: genero.nombre, email: newUser.email,
             password: newUser.password,
             fecha_de_nacimiento: newUser.fecha_de_nacimiento,
             altura: `${newUser.altura} ${unitsConfig.unidad_longitud}`,
@@ -115,44 +82,25 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-
-        if (!email || !password) {
-            return res.status(400).json({
-                message: "Invalid email or password"
-            });
-        }
+        if (!email || !password) return res.status(400).json({message: "Invalid email or password"});
 
         const userFound = await Usuario.findOne({ where: { email } });
-
-        if (!userFound) {
-            return res.status(400).json({
-                message: "User not found"
-            });
-        }
-
+        if (!userFound) return res.status(400).json({message: "User not found"});
+        
         console.log(`Password provided: ${password}`);
         console.log(`Password from the database: ${userFound.password}`);
-
-        const isMatch = await bcrypt.compare(password, userFound.password);
-        console.log(`Password comparison result: ${isMatch}`);
-
-        const token = await createAccessToken({ num_doc: userFound.num_doc, email: userFound.email });
-        res.cookie('token', token, { httpOnly: true, secure: true });
-
-        if (!isMatch) {
-            return res.status(400).json({
-                message: "Passwords did not match",
-                providedPassword: password,
-                hashedPassword: userFound.password,
-                token
-            });
+        
+        const token = await createAccessToken({ numDoc: userFound.num_doc });
+        res.cookie('token', token, { sameSite: 'none', httpOnly: true, secure: true });
+        
+        const passMatch = await bcrypt.compare(password, userFound.password);
+        if (!passMatch) {
+            return res.status(401).json({ message: 'La contraseña es incorrecta.',
+            providedPassword: password, hashedPassword: userFound.password,
+            token });
         }
 
-        res.json({
-            num_doc: userFound.num_doc,
-            email: userFound.email,
-            token
-        })
+        res.json({num_doc: userFound.num_doc, email: userFound.email, token})
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
@@ -231,7 +179,6 @@ const editProfile = async (req, res) => {
 
 const deleteProfile = async (req, res) => {
     try {
-        const usr_body = req.body;
         const user = await Usuario.destroy({
             where: { num_doc: req.params.num_doc }
         });
@@ -240,7 +187,7 @@ const deleteProfile = async (req, res) => {
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).send(
-                JSON.stringify({ is_deleted: user, usr_body },
+                JSON.stringify({ is_deleted: user ? true : false },
                 null, 2
             ));
         }
